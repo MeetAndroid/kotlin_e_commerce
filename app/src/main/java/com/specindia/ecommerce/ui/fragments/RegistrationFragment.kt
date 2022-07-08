c092bead1112cbf3330d3f806819919b1d346efd
NrupParikh <108717119+NrupParikh@users.noreply.github.com>
2022-07-08 19:22:54 +0530

    - Registration Screen Design

--- app/src/main/java/com/specindia/ecommerce/ui/fragments/RegistrationFragment.kt
+++ app/src/main/java/com/specindia/ecommerce/ui/fragments/RegistrationFragment.kt
@@ -1,11 +1,19 @@
 package com.specindia.ecommerce.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.specindia.ecommerce.R
 import com.specindia.ecommerce.databinding.FragmentRegistrationBinding
 
 class RegistrationFragment : Fragment() {
@@ -16,8 +24,30 @@ class RegistrationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentRegistrationBinding.inflate(layoutInflater)
-
+        setSpannableText()
         return binding.root
+    }
+
+    private fun setSpannableText() {
+        val spanText = SpannableStringBuilder(getString(R.string.already_have_an_account_login))
+        val clickableString = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                Toast.makeText(view.context, "MindOrks Clicked!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        spanText.setSpan(
+            clickableString,
+            0,
+            spanText.length,
+            0
+        )
+        spanText.setSpan(
+            ForegroundColorSpan(Color.RED),
+            25, // start
+            30, // end
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        binding.tvAlreadyHaveAnAccount.setText(spanText, TextView.BufferType.SPANNABLE)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,7 +60,7 @@ class RegistrationFragment : Fragment() {
             btnRegister.setOnClickListener {
                 view.findNavController().popBackStack()
             }
-            btnLogin.setOnClickListener {
+            tvAlreadyHaveAnAccount.setOnClickListener {
                 view.findNavController().popBackStack()
             }
         }
