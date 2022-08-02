b5d0140d61605cacdae97d8da803228e210b0f83
NrupParikh <108717119+NrupParikh@users.noreply.github.com>
2022-08-02 12:03:10 +0530

    - AutoFit Grid Layout Class added to support any number of column (span count) - Implemented in Food Menu Screen

---
+++ app/src/main/java/com/specindia/ecommerce/ui/activity/AutoFitGridLayoutActivity.kt
@@ -0,0 +1,34 @@
+package com.specindia.ecommerce.ui.activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.specindia.ecommerce.R
+import com.specindia.ecommerce.databinding.ActivityAutofitGridViewBinding
+import com.specindia.ecommerce.ui.adapters.NumberedAdapter
+import com.specindia.ecommerce.util.MarginDecoration
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AutoFitGridLayoutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAutofitGridViewBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAutofitGridViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val arrayList = ArrayList<String>()
+        for (i in 0..25) {
+            arrayList.add("$i")
+        }
+        Log.d("arrayList", arrayList.size.toString())
+        binding.apply {
+
+            autoFitRecyclerView.addItemDecoration(MarginDecoration(resources.getDimensionPixelSize(R.dimen.item_margin)))
+            autoFitRecyclerView.hasFixedSize()
+            autoFitRecyclerView.adapter = NumberedAdapter(arrayList)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/specindia/ecommerce/util/MarginDecoration.kt
@@ -0,0 +1,15 @@
+package com.specindia.ecommerce.util
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MarginDecoration(private val margin: Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect, view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State,
+    ) {
+        outRect[margin, margin, margin] = margin
+    }
+}
